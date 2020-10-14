7a1ff4b2418b66840888671777c0a4af7c52abfd
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-10-14 20:37:17 +0100

    Update 3-largest_prime_factor.js

--- 3-largest_prime_factor.js
+++ 3-largest_prime_factor.js
@@ -1,40 +1,33 @@
-let primeFactors = []
+/* Solution Function */
+let largestPrimeFactor = (number) => {
 
-let generatePrimeFactors = (number) => {
-
+  let largestPrime = 0
   // Quotient keeps track of result after division
   let quotient = number
 
   /* If Even, keep dividing by two */
   while(quotient % 2 === 0){
-    primeFactors.push(2)
-    quotient = quotient / 2
+      largestPrime = 2
+      quotient = quotient / 2
   }
 
   /* Quotient is odd at this point, so now find odd prime factors */
   let i
   // Look through odd numbers between 3 and quotient
-  for(i = 3; i < quotient; i = i + 2){
-    // If we can quotient by the odd number without a remainder, divide it
-    while(quotient % i === 0){
-      primeFactors.push(i)
-      quotient = quotient / i
-    }
-  }
-  // If the remaining quotient is a prime number, push to array
-  if(quotient !== 1){
-    primeFactors.push(quotient)
+  for(i = 3; i < Math.sqrt(quotient); i = i + 2){
+      // If we can quotient by the odd number without a remainder, divide it
+      while(quotient % i === 0){
+          largestPrime = i
+          quotient = quotient / i
+      }
   }
 
-}
+  if(quotient != 1){
+      largestPrime = quotient
+  }
 
-/* Solution Function */
-let largestPrimeFactor = (number) => {
 
-  generatePrimeFactors(number)
-
-  // Converts array into series of function arguments fed into Math.max
-  return Math.max(...primeFactors)
+  return largestPrime
 }
 
 
