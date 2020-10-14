d72e6edc8286662ee2d148d7b1ffb415816fb493
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-10-14 22:28:37 +0100

    Update 3-largest_prime_factor.js

--- 3-largest_prime_factor.js
+++ 3-largest_prime_factor.js
@@ -1,42 +1,31 @@
-let primeFactors = []
-
-let generatePrimeFactors = (number) => {
-
-  // Quotient keeps track of result after division
-  let quotient = number
-
-  /* If Even, keep dividing by two */
-  while(quotient % 2 === 0){
-    primeFactors.push(2)
-    quotient = quotient / 2
-  }
-
-  /* Quotient is odd at this point, so now find odd prime factors */
-  let i
-  // Look through odd numbers between 3 and quotient
-  for(i = 3; i < quotient; i = i + 2){
-    // If we can quotient by the odd number without a remainder, divide it
-    while(quotient % i === 0){
-      primeFactors.push(i)
-      quotient = quotient / i
-    }
-  }
-  // If the remaining quotient is a prime number, push to array
-  if(quotient !== 1){
-    primeFactors.push(quotient)
-  }
-
-}
-
-/* Solution Function */
 let largestPrimeFactor = (number) => {
 
-  generatePrimeFactors(number)
+  /* Keeps track of Latest Number we divided by */
+  let latestDivisor
+  /* Keeps track of result after division */
+  let quotient = number
 
-  // Converts array into series of function arguments fed into Math.max
-  return Math.max(...primeFactors)
+  /* If even, keep dividing by 2 */
+  while(quotient % 2 === 0){
+      latestDivisor = 2
+      quotient = quotient / 2
+      console.log(2)
+  }
+
+  /* Divide by smallest odd number possible
+      each time until quotient is 1 */
+  let divisor = 3
+  while(quotient > 1){
+      while(quotient % divisor === 0){
+          quotient = quotient / divisor
+          latestDivisor = divisor
+          console.log(divisor)
+      }
+      divisor = divisor + 2
+  }
+
+  return latestDivisor
+
 }
 
-
-/* Check Solution */
-console.log(largestPrimeFactor(600851475143))
+console.log('Result is ' + largestPrimeFactor(600851475143))
