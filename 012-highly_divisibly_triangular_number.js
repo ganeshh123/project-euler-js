7a8387f012ce3c9a86425e72fae2a2fc678abce2
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-10-21 17:30:05 +0100

    Create 012-highly_divisibly_triangular_number.js

---
+++ 012-highly_divisibly_triangular_number.js
@@ -0,0 +1,36 @@
+/* Solution Function */
+let divisibleTriangleNumber = (n) => {
+
+    /* For every divisior below root n, there is also one above root n */
+    let currentTriangular = 0
+    let count = 0
+
+    while(true){
+
+        /* Generate a new triangular number */
+        count = count + 1
+        currentTriangular = currentTriangular + count
+
+        /* Look for divisors between 2 and square root n, if we find one
+            there is also one above, so add 2 to the divisor count */
+
+        let divisorCount = 0
+        let i
+        for(i = 2; i < Math.sqrt(currentTriangular); i ++){
+            if(currentTriangular % i === 0){
+                divisorCount = divisorCount + 2;
+            }
+        }
+
+        console.log('Generated Triangular ' + currentTriangular + ' with ' + divisorCount + ' divisors.')
+
+        /* If number of divisors exceed n, then return the triangular number */
+        if(divisorCount > n){
+            return currentTriangular
+        }
+    }
+
+}
+
+/* Check Solution */
+console.log('Result is ' + divisibleTriangleNumber(500))
