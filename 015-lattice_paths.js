0962df771bda3efcd3a7ff85c9eade63da78e01d
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-11-17 21:54:12 +0000

    Create 015-lattice_paths.js

---
+++ 015-lattice_paths.js
@@ -0,0 +1,26 @@
+/* Factorial Function */
+let factorial = (number) => {
+
+    if(number === 1){
+        return 1;
+    }else{
+        return number * factorial(number - 1);
+    }
+}
+
+let latticePaths = (gridSize) => {
+
+    /* 
+        Based on Binomial Coffecient Forumula at 
+        https://en.wikipedia.org/wiki/Binomial_coefficient#Factorial_formula
+    */
+
+    n = 2 * gridSize // Number of Choices to make
+    k = gridSize // Number of times each choice can be made
+
+    /* n!/k!(n-k)! */
+    return factorial(n)/(factorial(k) * factorial(n-k));
+
+}
+/* Check Solution */
+console.log('Result is ' + latticePaths(4))
