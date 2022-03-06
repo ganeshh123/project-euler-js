bbf692df7229f9ed36009fd7529d840ba1fe8852
ganeshh123 <43777439+ganeshh123@users.noreply.github.com>
2022-03-06 23:44:51 +0000

    tidy max path sum

--- 018-maximum-path-sum-1.js
+++ 018-maximum-path-sum-1.js
@@ -6,27 +6,23 @@ let logTriangle = (triangle) => {
     console.log('----------')
 }
 
-
-
 let maximumPathSumI = (triangle) => {
-
     let row
     let column
 
     console.log('Initial ')
     logTriangle(triangle)
-
     console.log('')
     console.log('Trace :')
 
     /* Go through each row (array index ) from first to second-last row */
-    for(row = (triangle.length - 2); row >= 0; row --){
+    for (row = (triangle.length - 2); row >= 0; row--) {
 
         /* Go through each valid column for the row, number of columns equals row number in a 
             triangle structure */
-        for(column = 0; column <= row; column++){
-            let leftPathValue = triangle[row+1][column]
-            let rightPathValue = triangle[row+1][column+1]
+        for (column = 0; column <= row; column++) {
+            let leftPathValue = triangle[row + 1][column]
+            let rightPathValue = triangle[row + 1][column + 1]
             /* Add to Cell the value of the path below that has the max value */
             triangle[row][column] = triangle[row][column] + Math.max(leftPathValue, rightPathValue)
             logTriangle(triangle)
@@ -42,7 +38,7 @@ let triangle1 = [
     [7, 4, 0, 0],
     [2, 4, 6, 0],
     [8, 5, 9, 3]
-    ]
+]
 
 let triangle2 = [
     [75],
@@ -60,8 +56,8 @@ let triangle2 = [
     [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
     [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
     [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23],
-    ]
+]
 
 
 /* Check Solution */
-console.log('Result is ' + maximumPathSumI(triangle1))
+console.log('Result is ' + maximumPathSumI(triangle2))
